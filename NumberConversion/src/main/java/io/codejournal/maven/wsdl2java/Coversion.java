1d6f6474639b7f30ad0cfd964d22554d31e5dc66
FATIMA H NAQVI <hnaqvifatima@gmail.com>
2024-05-13 00:33:10 +0400

    e

---
+++ NumberConversion/src/main/java/io/codejournal/maven/wsdl2java/Coversion.java
@@ -0,0 +1,67 @@
+package io.codejournal.maven.wsdl2java;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Coversion {
+    public static void main(String[] args) throws MalformedURLException {
+        final String endpoint = "https://www.dataaccess.com/webservicesserver/numberconversion.wso";
+        final URL url = URI.create(endpoint).toURL();
+        final NumberConversion service = new NumberConversion(url);
+        final NumberConversionSoapType port = service.getPort(NumberConversionSoapType.class);
+        Scanner scanner = new Scanner(System.in);
+        int choice=-1;
+
+
+    do {
+            System.out.print("Choose:  \n1:Numbers To Dollar \n2:Numbers To Word \n3:Both \n4:Exit \n");
+
+                try {
+                    choice = Integer.parseInt(scanner.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid choice. Please enter a number.");
+                    continue;
+                }
+
+
+
+
+
+        if (choice != 1 && choice != 2 && choice != 3 && choice != 4 ) {
+            System.out.println("Invalid Choice");
+        }
+        if (choice == 1) {
+            convertToDollar(scanner, port);
+        }
+        else if (choice == 2) {
+            convertToDollar(scanner, port);
+        }
+        else if (choice == 3) {
+            convertToDollar(scanner, port);
+        }
+        else if (choice == 4) {
+            System.exit(0);
+        }
+       }while(choice != 4);
+
+}
+    private static void convertToDollar(Scanner scanner, NumberConversionSoapType port) {
+        System.out.print("Enter Number:");
+        String value = scanner.nextLine();
+        String dollar = port.numberToDollars(BigDecimal.valueOf(Long.parseLong(value)));
+        System.out.println("Number in Dollars: " + dollar);
+    }
+
+    private static void convertToWord(Scanner scanner, NumberConversionSoapType port) {
+        System.out.print("Enter Number:");
+        String value = scanner.nextLine();
+        String word = port.numberToWords(BigInteger.valueOf(Long.parseLong(value)));
+        System.out.println("Number in Words: " + word);
+    }
+}
+
+
