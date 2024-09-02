dc8cb2ee7c22917ec9628bf9f6e83a7196b22577
Fatima <hnaqvifatima@gmail.com>
2024-09-02 14:02:24 +0400

    Updated according to Nadia's comment

--- NumberConversion/src/main/java/io/codejournal/maven/wsdl2java/Coversion.java
+++ NumberConversion/src/main/java/io/codejournal/maven/wsdl2java/Coversion.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 
 public class Coversion {
     // TODO ZUNAID Throwing error without handling it
-<<<<<<< HEAD
     // FIXME Nadia: remove redundant throw from method
     //Fatima- Throw method removed
     // Fatima-with try-catch
@@ -18,12 +17,6 @@ public class Coversion {
         // FIXME Nadia: Redundant initializer. Make use of warnings
         //Fatima-Done
         URL url;
-=======
-    // Fatima-with try-catch
-    public static void main(String[] args) throws MalformedURLException {
-        final String endpoint = "https://www.dataaccess.com/webservicesserver/numberconversion.wso";
-        URL url = null;
->>>>>>> 24e8ac83edefcc16844d11d87bcaaa0f496d0e4c
         try {
             url = URI.create(endpoint).toURL();
         } catch (MalformedURLException e) {
@@ -31,22 +24,12 @@ public class Coversion {
             return; // Exit the program gracefully if URL creation fails
         }
         // TODO ZUNAID port is not a good use for a variable name here
-<<<<<<< HEAD
         // FIXME Nadia: Variable names need to be descriptive
         //Fatima-Changed the name of variable and made it more descriptive
         final NumberConversion service = new NumberConversion(url);
         final NumberConversionSoapType NumberConversionPort = service.getPort(NumberConversionSoapType.class);
         Scanner scanner = new Scanner(System.in);
         int choice;
-=======
-        //Fatima-Changed the name of variable
-        final NumberConversion service = new NumberConversion(url);
-        final NumberConversionSoapType NCD = service.getPort(NumberConversionSoapType.class);
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-
-
->>>>>>> 24e8ac83edefcc16844d11d87bcaaa0f496d0e4c
         do {
             // TODO ZUNAID Incorrect indentation here
             // TODO ZUNAID Number to words not implemented
@@ -57,7 +40,6 @@ public class Coversion {
             // Fatima- Switch has been used instead of if/else
             switch (choice) {
                 case 1:
-<<<<<<< HEAD
                     convertToDollar(scanner, NumberConversionPort);
                     break;
                 case 2:
@@ -66,16 +48,6 @@ public class Coversion {
                 case 3:
                     convertToDollar(scanner, NumberConversionPort);
                     convertToWord(scanner, NumberConversionPort);
-=======
-                    convertToDollar(scanner, NCD);
-                    break;
-                case 2:
-                    convertToWord(scanner, NCD);
-                    break;
-                case 3:
-                    convertToDollar(scanner, NCD);
-                    convertToWord(scanner, NCD);
->>>>>>> 24e8ac83edefcc16844d11d87bcaaa0f496d0e4c
                     break;
                 case 4:
                     System.exit(0);
@@ -84,16 +56,9 @@ public class Coversion {
                     System.out.println("Invalid Choice");
                     break;
             }
-
-<<<<<<< HEAD
         } while (choice != 4);
         // FIXME Nadia: remove whitespace
         //Fatima- Done
-=======
-        }while(choice != 4);
-        scanner.close();
-
->>>>>>> 24e8ac83edefcc16844d11d87bcaaa0f496d0e4c
     }
     private static void convertToDollar(Scanner scanner, NumberConversionSoapType NCD) {
         System.out.print("Enter Number:");
@@ -101,12 +66,8 @@ public class Coversion {
         String dollar = NCD.numberToDollars(BigDecimal.valueOf(Long.parseLong(value)));
         System.out.println("Number in Dollars: " + dollar);
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 24e8ac83edefcc16844d11d87bcaaa0f496d0e4c
     // TODO ZUNAID Number to words not implemented
-    // Fatima-Number to words implemented
+    // Fatima-Number to words implemented as well 
     private static void convertToWord(Scanner scanner, NumberConversionSoapType NCD) {
         System.out.print("Enter Number:");
         String value = scanner.nextLine();
@@ -114,5 +75,3 @@ public class Coversion {
         System.out.println("Number in Words: " + word);
     }
 }
-
-
